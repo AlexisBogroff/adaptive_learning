1b0b3e2e0b70614d7f17ee3643a44a21ff78f116
Richard Aumasson <50993056+Eminatek@users.noreply.github.com>
2021-02-01 09:23:47 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+"""
+Main settings file
+"""
+import os
+
+dirname = os.path.dirname(__file__)
+
+# Paths to db
+__PATH_EXAMS__ = os.path.join(dirname,"data/table_exams.txt")
+
+__PATH_QUESTIONS__ = os.path.join(dirname,"data/table_questions.txt")
+
+__PATH_TAB_SCHOOLS_INFOS__ = os.path.join(dirname,"data/table_schools_info.json")
+
